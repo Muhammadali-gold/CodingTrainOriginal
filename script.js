4bd27846b122957e100e8e74079412250520e44e
muhammadalinuri <>
2022-09-24 20:44:10 +0000

    Added Manderbolt challenge

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+// let colorlist = ['gold', 'yellow', 'turquoise', 'red']
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
-}
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   background(255);
+// }
 
-function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
-}
+// function draw() {
+//   noStroke()
+//   fill(random(colorlist));
+//   ellipse(mouseX, mouseY, 25, 25);
+// }
